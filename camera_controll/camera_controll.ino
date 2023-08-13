70b4248d19352ca340f18bb31df86b13e891e321
Don Mukisa <donmukisajoseph@gmail.com>
2023-08-13 14:24:50 +0300

    using TinyMLSheild Library

--- camera_controll/camera_controll.ino
+++ camera_controll/camera_controll.ino
@@ -1,34 +1,24 @@
-/*
- OV767X - Camera Capture Raw Bytes and Send to PC
+#include <TinyMLShield.h>
 
-  This sketch reads a frame from the OmniVision OV7675 camera
-  and sends the bytes to the PC via serial communication.
-*/
-
-#include <Arduino_OV767X.h>
-
+byte image[320*240];
 int bytesPerFrame;
 
-byte data[320 * 240 * 2]; // QVGA: 320x240 X 2 bytes per pixel (RGB565)
-
 void setup() {
-  //Serial.begin(115200); // Baud rate for serial communication
   Serial.begin(9600);
   while (!Serial);
 
-  if (!Camera.begin(QVGA, GRAYSCALE, 1)) {
-    Serial.println("Failed to initialize camera!");
+  initializeShield();
+
+  if (!Camera.begin(QVGA, GRAYSCALE, 1, OV7675)) {
+    Serial.println("Failed to initialize camera");
     while (1);
   }
-
   bytesPerFrame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
-
-  // Optionally, enable the test pattern for testing
-  // Camera.testPattern();
+  
+  Serial.println("Raw image data will begin streaming...");
 }
 
 void loop() {
-  Camera.readFrame(data);
-
-  Serial.write(data, bytesPerFrame);
+  Camera.readFrame(image);
+  Serial.write(image, bytesPerFrame); // Send live image data
 }
