e97bc5bcb60daae9e0b81f81b5f34f029915074b
Don Mukisa <donmukisajoseph@gmail.com>
2023-08-16 10:52:44 +0300

    included OCR in views and check plate function

--- PlateChecker/platesapp/tasks.py
+++ PlateChecker/platesapp/tasks.py
@@ -2,67 +2,10 @@
 from background_task import background
 from platesapp.models import Plate
 
-#-------------------------------------------OCR--------------------------------------------------
-import serial
-import numpy as np
-import cv2
-from google.cloud import vision_v1p4beta1 as vision
-#from google.protobuf.json_format import MessageToDict
-
-# Initialize the Google Cloud Vision client
-client = vision.ImageAnnotatorClient()
-
-ser = serial.Serial('COM5', 9600)
-
-def receive_image(width, height):
-    image_data = ser.read(width * height)
-    image_array = np.frombuffer(image_data, dtype=np.uint8)
-    return image_array.reshape((height, width))
-
-def detect_objects(image):
-    # Convert the image array to bytes
-    _, image_bytes = cv2.imencode('.jpg', image)
-
-    # Perform object detection using Google Cloud Vision API
-    image = vision.Image(content=image_bytes.tobytes())
-    response = client.object_localization(image=image)
-    objects = response.localized_object_annotations
-
-    # Extract object labels (if available)
-    object_labels = []
-    for obj in objects:
-        object_labels.append(obj.name)
-
-    return object_labels
-
-
-width, height = 320, 240
-while True:
-    image = receive_image(width, height)
-
-    # Perform object detection and label extraction
-    object_labels = detect_objects(image)
-
-    # Convert the image array back to an image
-    received_image = np.array(image, dtype=np.uint8)
-
-    # Display the received image
-    cv2.imshow('Received Image', received_image)
-
-    # Print detected object labels
-    if object_labels:
-            print('Detected Objects:', ', '.join(object_labels))
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-ser.close()
-cv2.destroyAllWindows()
-
-
-#-------------------------------------------OCR--------------------------------------------------
 @background(schedule=1)  # Check every 1 second
-def check_plate_in_database(value):
-    if Plate.objects.filter(plates=value).exists():
-        # Perform some action, e.g., send notification
-        pass
+def check_plate_in_database(list_of_labels):
+    for label in list_of_labels:
+        if Plate.objects.filter(plates=label).exists():
+            # Perform some action, e.g., send notification
+            
+            pass
