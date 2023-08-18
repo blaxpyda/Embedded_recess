03ff0fb6f0e78928393730854ad6358d6877c76c
Don Mukisa <donmukisajoseph@gmail.com>
2023-08-18 16:26:22 +0300

    updated text recognition

--- tasks1.py
+++ tasks1.py
@@ -33,53 +33,45 @@ def check_plate_in_database(*args, **kwargs):
         
         return image_array.reshape((height, width))
 
+    def detect_objects(image):
+        # Convert the image array to bytes
+        _, image_bytes = cv2.imencode('.jpg', image)
+
+        # Perform object detection using Google Cloud Vision API
+        image = vision.Image(content=image_bytes.tobytes())
+        response = client.object_localization(image=image)
+        objects = response.localized_object_annotations
+
+        # Extract object labels (if available)
+        object_labels = []
+        for obj in objects:
+            object_labels.append(obj.name)
+
+        return object_labels
+
+    print('00000000000000000000000000000000000000000000000000000000')
     width, height = 320, 240
-    
+
     image = receive_image(width, height)
 
-        # Convert the image array to bytes
-    _, image_bytes = cv2.imencode('.jpg', image)
+        # Perform object detection and label extraction
+    object_labels = detect_objects(image)
 
-        # Perform text detection using Google Cloud Vision API
-    image = vision.Image(content=image_bytes.tobytes())
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
+        # Convert the image array back to an image
+    received_image = np.array(image, dtype=np.uint8)
 
-        # Convert the image bytes back to a NumPy array
-    nparr = np.frombuffer(image_bytes, dtype=np.uint8)
-    received_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-        # Display the received image with detected text
+        # Display the received image
     cv2.imshow('Received Image', received_image)
     cv2.imwrite('output_image.jpg', received_image)  # Replace 'output_image.jpg' with the desired output path and filename
 
-        # Print extracted text
-    if texts:
-        for text in texts:
-            text.description = ''+ text.description
-            if Plate.objects.filter(plates=text.description).exists():
-            # Perform some action, e.g., send notification
-                
-                send_mail(
-                'ALERT A MATCHING PLATE FOUND',
-                f'The Mtaching Plate : {text.description}',
-                'donmukisajoseph@gmail.com',
-                ['arindaj33@gmail.com','zbagabo@gmail.com','donjosephmukisa@gmail.com','donmukisajoseph@gmail.com'],
-                fail_silently=False,
-                )
-                print('Email Sent')
-                print(text.description)
-            
-            #print(text.description)
-        text.description = ''
-
+        # Print detected object labels
+    if object_labels:
+            print('Detected Objects:', ', '.join(object_labels))
+    print('111111111111111111111111111111111111111111111111111111111111111')
+    
     ser.close()
     cv2.destroyAllWindows()
-
     
-    
-
-    '''  
     for label in object_labels:
         print('2222222222222222222222222222222222222222222222222222222222222')
         if Plate.objects.filter(plates=label).exists():
@@ -95,6 +87,3 @@ def check_plate_in_database(*args, **kwargs):
             print('Email Sent')
             print(label)
             
-'''
-
-
