4899fd87221245300a04b404b9b465e632a4a8b7
Don Mukisa <donmukisajoseph@gmail.com>
2023-08-21 17:18:42 +0300

    all

--- PlateChecker/platesapp/urls.py
+++ PlateChecker/platesapp/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('', views.login_view, name='login'),
     path('car-registration/', views.car_registration_view, name='car_registration'),
+    path('admin-dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('dashboard/', views.user_dashboard_view, name='user_dashboard'),
+    
     
 ]
--- PlateChecker/platesapp/views.py
+++ PlateChecker/platesapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from .forms import CarRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from .models import Car,PendingNumberPlate, DeletedNumberPlate, ActivatedNumberPlate
 # Create your views here.
 from .models import Plate
 from .tasks import check_plate_in_database
@@ -30,7 +31,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dashboard')  # Replace 'dashboard' with the actual URL name for the dashboard page
+            return redirect('login')  # Redirect to login page after successful signup
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
@@ -39,13 +40,20 @@ def signup_view(request):
 
 
 #login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('dashboard')  # Replace 'dashboard' with the actual URL name for the dashboard page
+            if user.is_staff:
+                return redirect('admin_dashboard')  # Redirect to admin dashboard
+            else:
+                return redirect('user_dashboard')  # Redirect to normal user dashboard
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
@@ -62,7 +70,7 @@ def car_registration_view(request):
             car = form.save(commit=False)
             car.owner = request.user
             car.save()
-            return redirect('dashboard')  # Replace 'dashboard' with the actual URL name for the dashboard page
+            return redirect('user_dashboard')  # Replace 'dashboard' with the actual URL name for the dashboard page
     else:
         form = CarRegistrationForm()
     return render(request, 'car_registration.html', {'form': form})
@@ -70,9 +78,32 @@ def car_registration_view(request):
 
 
 
+
+
 @staff_member_required
 def admin_dashboard_view(request):
-    return render(request, 'admin_dashboard.html')
+    pending_number_plates = PendingNumberPlate.objects.all()
+    deleted_number_plates = DeletedNumberPlate.objects.all()
+    users_with_number_plates = []
 
+    for user in User.objects.all():
+        user_info = {'user': user, 'number_plates': Car.objects.filter(owner=user)}
+        users_with_number_plates.append(user_info)
+
+    context = {
+        'pending_number_plates': pending_number_plates,
+        'deleted_number_plates': deleted_number_plates,
+        'users_with_number_plates': users_with_number_plates
+    }
+    return render(request, 'admin_dashboard.html', context)
+
+
+
+
+@login_required
+def user_dashboard_view(request):
+    user_cars = Car.objects.filter(owner=request.user)
+    context = {'user_cars': user_cars}
+    return render(request, 'user_dashboard.html', context)
 
 check_plate_in_database()
