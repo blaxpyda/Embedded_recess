34d2a5948670f02f418b1060a87ce144d8183a4b
Don Mukisa <donmukisajoseph@gmail.com>
2023-08-21 16:34:37 +0300

    migrations

--- PlateChecker/platesapp/urls.py
+++ PlateChecker/platesapp/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('insert/', views.insert_plate, name='insert_plate'),
     path('display/', views.display_table, name='display_table'),
-    path('', views.insert_plate, name='insert_plate'),
+    #path('', views.insert_plate, name='insert_plate'),
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('car-registration/', views.car_registration_view, name='car_registration'),
     
 ]
